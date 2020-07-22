e51fbf97bb9fe6bde8d5758418018332508a7726
Ayman91 <ayman.mosstafa@gmail.com>
2020-07-22 05:31:09 +0200

    feat: creating tasks

---
+++ app.js
@@ -0,0 +1,21 @@
+let addIcon = document.querySelector('.fa-plus')
+const input = document.querySelector('input');
+
+// EventListeners
+addIcon.addEventListener('click', createTask);
+
+// creating task
+createTask = () => {
+    const taskList = document.querySelector('.task-list');
+    const taskItemm = document.createElement('div');
+    taskList.appendChild(taskItemm);
+    taskItemm.innerHTML =
+        `<div class="task-item">
+        <i class="arrange-task fas fa-list-ul"></i>
+        <p type="text" class="task-title">${input.value}</p>
+        <i class="completed fa-lg fas fa-check-square"></i>
+        <i class="edit fa-lg fas fa-pen-square"></i>
+        <i class="delete fa-lg fas fa-minus-square"></i>
+    </div>`
+    input.value = '';
+};
